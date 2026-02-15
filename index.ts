b07dd1442c307af39b37c212cf24f2e0b0d2b3d3
Bengt Brodersen <bengt.brodersen@gmail.com>
2026-02-15 23:01:33 +0100

    Refactor octokit initialization and add GITHUB_API_URL
    Set GITHUB_API_URL based on GITHUB_SERVER_URL if not defined.

--- index.ts
+++ index.ts
@@ -6,15 +6,17 @@ import * as process from 'node:process'
 import {sleep} from "./lib/common.js";
 import * as github from "@actions/github";
 
+if(!process.env.GITHUB_API_URL && process.env.GITHUB_SERVER_URL) {
+  process.env.GITHUB_API_URL = `${process.env.GITHUB_SERVER_URL.replace(/\/$/, '')}/api/v3`
+}
+
 export const action = run(async () => {
 
   const inputs = {
     token: getInput('token', {required: true}),
   }
 
-  const octokit = github.getOctokit(inputs.token, {
-    baseUrl: process.env.GITHUB_SERVER_URL || 'https://github.com',
-  });
+  const octokit = github.getOctokit(inputs.token);
 
   // --- due to some eventual consistency issues with the GitHub API, we need to take a sort break
   await sleep(2000)
